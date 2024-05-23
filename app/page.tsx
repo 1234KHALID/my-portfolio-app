62c16e192f938ce771de6a57f8b454f1c400ce74
Mohd Khalid <mohammad@prodios.com>
2024-05-23 15:43:37 +0530

    add about section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
+import About from "@/components/about";
 
 const Home = () => {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
+      <SectionDivider />
+      <About />
     </main>
   );
 };
